599864b92bc2baa01ea7971eff9be94a0e40f151
Wlad <w.pylypczuk@gmail.com>
2022-01-26 18:03:23 -0800

    cnit133

---
+++ cnit133/gethint.php
@@ -0,0 +1,66 @@
+<?php
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+$a[] = "Amelia";
+$a[] = "Alexander";
+$a[] = "Emma";
+$a[] = "Emily";
+$a[] = "Layla";
+$a[] = "Lucas";
+$a[] = "Oliver";
+$a[] = "Sophia";
+$a[] = "Sebastian";
+$a[] = "Kevin";
+$a[] = "Kacy";
+$a[] = "Wales";
+$a[] = "Walker";
+$a[] = "Zendaya";
+
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+if ($q !== "") {
+  $q = strtolower($q);
+  $len=strlen($q);
+  foreach($a as $name) {
+    if (stristr($q, substr($name, 0, $len))) {
+      if ($hint === "") {
+        $hint = $name;
+      } else {
+        $hint .= ", $name";
+      }
+    }
+  }
+}
+
+echo $hint === "" ? "no suggestion" : $hint;
+?>
